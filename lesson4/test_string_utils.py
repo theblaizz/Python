317379e7ae8bd7611d6d507b4052682e9e24998a
theblaizz <theblaizz@gmail.com>
2025-11-04 12:45:50 +0300

    lesson4

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -42,3 +42,41 @@ def test_trim_positive(input_str, expected):
 def test_trim_negative(input_str, expected):
     assert string_utils.trim(input_str) == expected
 
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("S","S", True),
+    (" Wer"," Wer", True),
+    ("   Санкт-петербург","Санкт", True), ])
+def test_contains_positive(input_str,symbol, expected):
+    assert string_utils.contains(input_str,symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str,symbol, expected", [
+    ("S", "T", False),
+    (" Wer", " wer", False),
+    ("   Санкт-петербург", "мос", False), ])
+
+def test_contains_negative(input_str,symbol, expected):
+    assert string_utils.contains(input_str,symbol) == expected
+
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("S","S", ""),
+    (" Wer"," Wer", ""),
+    ("   Санкт-петербург","Санкт", "   -петербург"), ])
+def test_delete_symbol_positive(input_str,symbol, expected):
+    assert string_utils.delete_symbol(input_str,symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str,symbol, expected", [
+    ("S", "T", "S"),
+    (" Wer", " wer",  " Wer"),
+    ("   Санкт-петербург", "мос", "   Санкт-петербург"), ])
+
+def test_delete_symbol_negative(input_str,symbol, expected):
+    assert string_utils.delete_symbol(input_str,symbol) == expected
+
